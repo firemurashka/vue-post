758abc747d7caa903823add7ecb0ff4ee7394fbd
firemurashka <firemurashka@gmail.com>
2022-07-19 11:00:13 +0300

    создание my-select

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,17 @@
+<template>
+	<select>
+		<option disabled value="">Выберите из списка</option>
+	</select>
+</template>
+
+
+<script>
+export default {
+	name: "my-select"
+}
+</script>
+
+
+
+<style>
+</style>
