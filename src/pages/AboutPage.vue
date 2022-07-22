d759c432417a45a6812506d306f516d74e83ccd5
firemurashka <firemurashka@gmail.com>
2022-07-22 12:15:37 +0300

    router-link - создание ссылок

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,83 @@
+<template>
+	<div>
+		<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, nobis dolore. Veritatis excepturi sunt maxime?
+			Nesciunt obcaecati quibusdam soluta voluptate ea ullam molestiae atque iusto adipisci cum! Possimus, aspernatur
+			officiis doloremque sint non explicabo officia. Voluptatem, molestiae commodi labore perferendis sequi, amet
+			facilis eveniet fugit rem quia inventore pariatur, quas ullam sint culpa laudantium. Beatae accusamus tenetur
+			necessitatibus? Rerum natus ipsum culpa voluptas beatae minus cumque eius alias, consequuntur neque quod amet!
+			Hic ea dignissimos voluptatibus tempore nostrum, beatae, a est mollitia, officiis facere quidem deserunt?
+			Laboriosam mollitia sequi earum distinctio rem iste laborum placeat similique molestias maiores obcaecati
+			dolores suscipit accusamus pariatur facere quae, necessitatibus autem sapiente est harum, natus quaerat unde
+			tempora. Magnam laudantium soluta doloribus possimus similique quo praesentium harum maxime consequatur
+			tempora, cumque ea delectus illum vero? Voluptatem quis vel sunt numquam voluptatum optio nesciunt sapiente
+			suscipit eligendi aliquid distinctio nemo minus ipsum veniam molestiae facilis est, expedita id nihil totam
+			tenetur nisi praesentium quisquam? Magnam sapiente, officia eligendi minus nesciunt consequuntur voluptates non
+			praesentium delectus! Quasi suscipit eum laboriosam cupiditate dolor numquam porro delectus dolorum, natus
+			voluptas magnam accusamus neque asperiores. Quis architecto, odio laborum, dolores aliquid incidunt aspernatur
+			natus facere necessitatibus dolor possimus fugiat praesentium vero? Alias, atque neque. Repellat, quia minus
+			dicta, quos ratione cum sit saepe voluptatem distinctio mollitia in, explicabo assumenda. Aut libero voluptas
+			nulla quisquam odit maiores debitis? At sapiente laboriosam, excepturi aspernatur quis quas corrupti ad
+			adipisci deserunt, necessitatibus facilis, quidem sit? Quaerat iste, repellendus voluptas animi exercitationem
+			inventore at illo est doloremque sequi temporibus laboriosam. Qui deleniti officia veritatis nobis quos vel
+			aliquid molestiae unde voluptas voluptatum sunt maxime recusandae, architecto itaque laboriosam, tempora
+			voluptates quisquam quam nulla quas? Deleniti quis illo mollitia aut neque iusto provident quia quod blanditiis
+			voluptatum, labore excepturi inventore, hic asperiores modi libero odit est rerum corporis ipsam, dicta
+			repellendus eius velit esse! Deserunt quas atque quia commodi neque! Ad quasi deserunt quia. Beatae perferendis
+			eligendi rerum odit quae, quibusdam in numquam accusamus magnam dolor laborum facere facilis at voluptatum non
+			et. Provident cumque explicabo voluptas voluptates minima ex accusamus soluta perspiciatis animi repudiandae
+			aliquid aliquam officiis pariatur consectetur, enim, aperiam doloremque voluptate modi doloribus tempora neque
+			eligendi quae mollitia! Earum dolor molestias obcaecati aspernatur repellendus nobis aliquid, animi quam
+			dolores mollitia rerum, tempora doloremque culpa cumque, ex cum. Excepturi dignissimos omnis facilis obcaecati
+			nulla veritatis? Sunt ex tenetur quisquam perspiciatis saepe reprehenderit repellat deleniti accusamus! Placeat
+			assumenda beatae, optio quasi maiores quaerat deserunt doloribus exercitationem quae nam est, nostrum natus
+			earum consectetur laborum quod eveniet! Nemo quos pariatur delectus dicta, veritatis dolore tempora praesentium
+			distinctio aliquam molestias nam tempore excepturi laudantium officiis, maiores alias ab voluptate, saepe totam
+			nostrum dignissimos. Nesciunt maiores ex pariatur. Voluptatem laudantium iste quae nihil accusantium earum
+			ducimus est blanditiis distinctio harum repellat ipsa pariatur temporibus omnis impedit, nemo ab asperiores,
+			commodi quidem aperiam similique. Quas dolores illo deserunt alias mollitia ad, at laborum repellendus placeat
+			doloremque commodi minima debitis assumenda magnam ullam blanditiis eum porro! Sit repudiandae nulla totam
+			aperiam nemo libero, omnis cupiditate non vitae placeat nobis sunt dolorem tenetur reprehenderit sequi illo
+			excepturi eligendi nostrum tempora fugit saepe odit. Maxime earum laudantium error illum, debitis natus quas
+			quo doloribus minima fugit quasi aut obcaecati voluptatum distinctio delectus. Deleniti doloribus impedit quo
+			quibusdam doloremque! Obcaecati et culpa rem fuga velit assumenda ab, fugiat amet iste delectus sapiente
+			ratione non officia, earum corporis sit numquam excepturi, consectetur accusamus vitae autem est. Cumque quasi
+			recusandae nemo veritatis ex consequatur autem ut vel sed illum, quis voluptates officia, quisquam quae earum
+			necessitatibus quaerat quo, fugit culpa quam nam quos? Velit, minima. Quaerat impedit vel maiores tenetur quae?
+			Quis omnis quam repudiandae aperiam quibusdam harum modi reiciendis quaerat, iste at placeat temporibus ducimus
+			nulla cupiditate possimus autem vitae molestias accusantium voluptatibus a nesciunt. Dolore animi enim,
+			excepturi quis vel veritatis facere incidunt aperiam reiciendis quaerat voluptatem nobis libero nemo quia
+			blanditiis dolorum recusandae accusantium! Sunt dolore deleniti tempore ad, numquam cumque odio quo enim, dolor
+			deserunt dolores perspiciatis accusamus libero ea ex id dolorem sapiente laborum non debitis, similique quae
+			possimus iste. Quam voluptate ducimus vitae libero iste ipsam perspiciatis dicta molestias quidem voluptatem,
+			eveniet itaque id facere, suscipit saepe natus, nostrum et praesentium velit? Nesciunt commodi molestias
+			repellat ullam distinctio! Facilis dolores, incidunt rem dolore officiis accusamus enim eos odio id saepe
+			laboriosam perspiciatis maxime non dolorem suscipit alias eligendi quaerat eum quasi est magni. Provident rem
+			neque laudantium officia sit aspernatur dicta amet delectus? Hic vel et beatae iusto nulla autem aperiam cum
+			assumenda doloremque laudantium deleniti, ratione quam perferendis voluptatum laborum atque placeat modi totam
+			odio tempora rerum. Itaque voluptas odit omnis, tenetur qui vero, sequi tempora expedita non quos aut. A ullam,
+			iusto est possimus impedit officiis nulla eos nisi tenetur dignissimos, rem aspernatur? Quo at similique magni
+			quisquam incidunt illum excepturi deserunt unde nam tempora obcaecati non sed ipsa animi, accusamus impedit
+			doloribus recusandae ut modi asperiores numquam delectus enim magnam. Inventore repellendus iste officiis sequi
+			qui neque animi. Itaque facere doloribus dolor, eveniet deleniti accusantium asperiores. Ex eligendi voluptatum
+			ea distinctio labore commodi, laudantium officiis harum ad id natus molestias mollitia laboriosam excepturi
+			recusandae perferendis provident quia blanditiis repellat consectetur accusamus consequuntur. Nobis est a eaque
+			voluptates tenetur, libero ducimus. Delectus eos hic ab nihil in cupiditate a, iure quasi enim reiciendis vero
+			minima, sit nemo velit tempore omnis? Consequuntur dolorem distinctio repudiandae ab repellendus facilis,
+			quaerat corrupti consectetur aut amet et nulla vitae explicabo iste molestiae, veniam vero temporibus sequi
+			debitis odio eaque est tempore laudantium quam! Beatae, vitae fuga. Dolore porro magni eum eaque amet
+			cupiditate, quas perferendis reiciendis officiis molestias laudantium debitis aspernatur ex quae, odit adipisci
+			fugiat saepe sunt dicta, soluta delectus obcaecati? Aperiam, tempore saepe perspiciatis iure vitae suscipit
+			molestiae quo quibusdam repellendus porro distinctio, impedit odit dignissimos facilis earum nisi incidunt
+			laboriosam minus dolorem? Beatae libero obcaecati provident incidunt, repellat dicta, eos distinctio, atque
+			sunt asperiores aut omnis dolorem! Assumenda iste pariatur cum veniam ullam recusandae veritatis in.!</h1>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,22 @@ import {
     createRouter,
     createWebHistory
 } from "vue-router";
+import PostPage from "@/pages/PostPage";
+import AboutPage from "@/pages/AboutPage";
 
 const routes = [{
-    path: '/',
-    component: MainPage
-}]
+        path: '/',
+        component: MainPage
+    },
+    {
+        path: '/posts',
+        component: PostPage
+    },
+    {
+        path: '/about',
+        component: AboutPage
+    },
+]
 
 
 const router = createRouter({
